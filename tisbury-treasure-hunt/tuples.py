8cc441e707c71b1db601fe152c5e87134edd93a6
Anshul Prasad <anshulprasad5440.com>
2025-11-29 16:00:58 +0530

    completed

--- tisbury-treasure-hunt/tuples.py
+++ tisbury-treasure-hunt/tuples.py
@@ -8,7 +8,7 @@ def get_coordinate(record):
     :return: str - the extracted map coordinate.
     """
 
-    pass
+    return record[1]
 
 
 def convert_coordinate(coordinate):
@@ -18,7 +18,7 @@ def convert_coordinate(coordinate):
     :return: tuple - the string coordinate split into its individual components.
     """
 
-    pass
+    return tuple(coordinate)
 
 
 def compare_records(azara_record, rui_record):
@@ -29,7 +29,7 @@ def compare_records(azara_record, rui_record):
     :return: bool - do the coordinates match?
     """
 
-    pass
+    return convert_coordinate(get_coordinate(azara_record)) == rui_record[1]
 
 
 def create_record(azara_record, rui_record):
@@ -40,7 +40,10 @@ def create_record(azara_record, rui_record):
     :return: tuple or str - the combined record (if compatible), or the string "not a match" (if incompatible).
     """
 
-    pass
+    if compare_records(azara_record, rui_record):
+        return azara_record + rui_record
+    else:
+        return "not a match"
 
 
 def clean_up(combined_record_group):
@@ -54,4 +57,10 @@ def clean_up(combined_record_group):
     (see HINTS.md for an example).
     """
 
-    pass
+    return (
+        "\n".join(
+            str(tuple([combined_record_group[i][0]]) + combined_record_group[i][2:])
+            for i in range(len(combined_record_group))
+        )
+        + "\n"
+    )
