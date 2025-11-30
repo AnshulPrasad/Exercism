70c4786795eea5ba1c13dcabf49120122a967d74
Anshul Prasad <anshulprasad5440.com>
2025-11-30 17:19:38 +0530

    complete

--- grains/grains.py
+++ grains/grains.py
@@ -1,6 +1,9 @@
 def square(number):
-    pass
+
+    if not 1 <= number <= 64:
+        raise ValueError("square must be between 1 and 64")
+    return pow(2, number - 1)
 
 
 def total():
-    pass
+    return sum(square(num) for num in range(1, 65))
