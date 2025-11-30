6a1faa864d20c556dc56441bc154346d67198ce4
Anshul Prasad <anshulprasad5440.com>
2025-11-30 17:08:59 +0530

    completed

--- triangle/triangle.py
+++ triangle/triangle.py
@@ -1,10 +1,26 @@
 def equilateral(sides):
-    pass
+    return sides[0] == sides[1] == sides[2] != 0
 
 
 def isosceles(sides):
-    pass
+    a, b, c = sides
+    if a == 0 or b == 0 or c == 0:  # non-zero sides
+        return False
+    if a + b >= c and b + c >= a and a + c >= b:  # valid triangles
+        return a == b or b == c or c == a  # is isosceles
+    else:
+        return False
 
 
 def scalene(sides):
-    pass
+    a, b, c = sides
+
+    if a == 0 or b == 0 or c == 0:  # non-zero sides
+        return False
+
+    if a + b >= c and b + c >= a and a + c >= b:  # valid triangle
+        return a != b and b != c and c != a  # is scalen
+    else:
+        return False
+
+    return a != b and a != c and b != c
