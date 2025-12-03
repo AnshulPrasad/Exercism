43bc725c455cec709920d2c3b0baec9ee102fa64
Anshul Prasad <anshulprasad5440.com>
2025-12-03 12:37:42 +0530

    completed

--- raindrops/raindrops.py
+++ raindrops/raindrops.py
@@ -1,2 +1,6 @@
 def convert(number):
-    pass
+    ans = 'Pling'*(not bool(number%3)) + 'Plang'*(not bool(number%5)) + 'Plong'*(not bool(number%7)) + ""
+    if ans == "":
+        ans += str(number)
+    return ans
+    
