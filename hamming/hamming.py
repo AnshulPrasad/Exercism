a214ddca4146f4accad7c44fae62b68552134046
Anshul Prasad <anshulprasad5440.com>
2025-12-05 20:22:21 +0530

    completed

--- hamming/hamming.py
+++ hamming/hamming.py
@@ -1,2 +1,8 @@
 def distance(strand_a, strand_b):
-    pass
+    if len(strand_a) != len(strand_b):
+        raise ValueError("Strands must be of equal length.")
+    counter = 0
+    for i, j in zip(strand_a, strand_b):
+        if i != j:
+            counter += 1
+    return counter
