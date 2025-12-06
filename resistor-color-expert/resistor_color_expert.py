8d9c58996ec74dae0c931123b4218124a05899d4
Anshul Prasad <anshulprasad5440.com>
2025-12-06 15:23:49 +0530

    completed

--- resistor-color-expert/resistor_color_expert.py
+++ resistor-color-expert/resistor_color_expert.py
@@ -1,2 +1,42 @@
 def resistor_label(colors):
-    pass
+    color_band = {
+        "black": "0",
+        "brown": "1",
+        "red": "2",
+        "orange": "3",
+        "yellow": "4",
+        "green": "5",
+        "blue": "6",
+        "violet": "7",
+        "grey": "8",
+        "white": "9",
+    }
+
+    tolerance_band = {
+        "grey": "0.05",
+        "violet": "0.1",
+        "blue": "0.25",
+        "green": "0.5",
+        "brown": "1",
+        "red": "2",
+        "gold": "5",
+        "silver": "10",
+    }
+    ans = ""
+    if len(colors) == 1:
+        return f"{color_band[colors[0]]} ohms"
+    elif len(colors) == 2:
+        return f"{color_band[colors[0]]}{color_band[colors[1]]} ohms"
+    elif len(colors) == 3:
+        return f"{color_band[colors[0]]}{color_band[colors[1]]}{color_band[colors[2]]} ohms"
+
+    for i in colors[:-2]:
+        ans += color_band[i]
+    ans += "0" * int(color_band[colors[-2]])
+
+    if len(ans) <= 3:
+        return f"{ans} ohms ±{tolerance_band[colors[-1]]}%"
+    elif 3 < len(ans) <= 6:
+        return f"{int(ans)/1000:g} kiloohms ±{tolerance_band[colors[-1]]}%"
+    elif 6 < len(ans) <= 9:
+        return f"{int(ans)/1000000:g} megaohms ±{tolerance_band[colors[-1]]}%"
