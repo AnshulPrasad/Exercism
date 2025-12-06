7e60ed62a296272e0a3fb8093499000386ba537b
Anshul Prasad <anshulprasad5440.com>
2025-12-06 14:01:02 +0530

    completed

--- rotational-cipher/rotational_cipher.py
+++ rotational-cipher/rotational_cipher.py
@@ -1,2 +1,11 @@
 def rotate(text, key):
-    pass
+    ans = ""
+    for i in text:
+        if not i.isalpha():
+            ans += i
+            continue
+        char = "a"
+        if i.isupper():
+            char = "A"
+        ans += chr((ord(i) - ord(char) + key % 26) % 26 + ord(char))
+    return ans
