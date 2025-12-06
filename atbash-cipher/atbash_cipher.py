6f325c228d52da59aa134e267933be80aa6ab2c7
Anshul Prasad <anshulprasad5440.com>
2025-12-06 16:30:12 +0530

    completed

--- atbash-cipher/atbash_cipher.py
+++ atbash-cipher/atbash_cipher.py
@@ -1,6 +1,14 @@
 def encode(plain_text):
-    pass
+    plain = "abcdefghijklmnopqrstuvwxyz"
+    ans = ""
+    for i in plain_text:
+        if i.isalpha():
+            ans += plain[25 - (ord(i.lower()) - ord("a"))]
+        elif i.isdigit():
+            ans += i
+    ans = " ".join([ans[i : i + 5] for i in range(0, len(ans), 5)])
+    return ans
 
 
 def decode(ciphered_text):
-    pass
+    return encode(ciphered_text).replace(" ", "")
