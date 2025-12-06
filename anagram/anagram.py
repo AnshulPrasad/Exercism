ae3d58580db4eed39758cc794362fbbab61663a7
Anshul Prasad <anshulprasad5440.com>
2025-12-06 15:53:43 +0530

    completed

--- anagram/anagram.py
+++ anagram/anagram.py
@@ -1,2 +1,12 @@
+from collections import Counter
+
+
 def find_anagrams(word, candidates):
-    pass
+    ans = []
+    for i in candidates:
+        if (
+            Counter(list(i.lower())) == Counter(list(word.lower()))
+            and i.lower() != word.lower()
+        ):
+            ans.append(i)
+    return ans
